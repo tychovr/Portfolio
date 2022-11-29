1651bb23c445c36e68f08c521ee440babbf29c27
tychovr <68756557+tychovr@users.noreply.github.com>
2022-11-29 14:01:32 +0100

    Added - [404 Page] - No design/styling yet

---
+++ src/pages/notfound/notfound.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1 style={{color: "red"}}>Oops! You seem to be lost.</h1>
+    </div>
+  );
+}
