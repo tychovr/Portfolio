66813db5086ff33f0fc618009fa89838a3d6993b
tychovr <68756557+tychovr@users.noreply.github.com>
2022-12-08 21:18:03 +0100

    Added - [404 Page] - No more wanderers

--- src/pages/notfound/notfound.tsx
+++ src/pages/notfound/notfound.tsx
@@ -1,7 +1,30 @@
-export default function NotFound() {
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./notfound.scss";
+
+const NotFound = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    document.title = "404 | Tycho van Rosmalen";
+  }, []);
+
   return (
-    <div>
-      <h1 style={{color: "red"}}>Oops! You seem to be lost.</h1>
+    <div className="notfound-container">
+      <div className="notfound-content">
+        <div className="notfound">
+          <h1>404</h1>
+          <h2>Oops! You seem to be lost.</h2>
+          <p>
+            The page you are looking for does not exist. How you got here is a
+            mystery. But you can click the button below to go back to the
+            homepage.
+          </p>
+          <button onClick={() => navigate("/")}>Go Back</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default NotFound;
