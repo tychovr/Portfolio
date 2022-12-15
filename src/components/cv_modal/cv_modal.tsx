409bbfbb9a92f5a295ef5c9bc788fe5c376a41de
tychovr <68756557+tychovr@users.noreply.github.com>
2022-12-15 19:27:58 +0100

    Removed - [Move Feature] - Caused lower UX

--- src/components/cv_modal/cv_modal.tsx
+++ src/components/cv_modal/cv_modal.tsx
@@ -18,12 +18,6 @@ const dropIn = {
   visible: {
     y: "0",
     opacity: 1,
-    transition: {
-      duration: 0.1,
-      type: "spring",
-      damping: 25,
-      stiffness: 500,
-    },
   },
   exit: {
     y: "100vh",
